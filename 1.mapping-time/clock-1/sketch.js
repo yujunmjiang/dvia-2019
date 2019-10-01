bf610e37d93d47872431c93d7390b43310579bf5
Yujun Jiang <yujunmjiang@gmail.com>
2019-10-01 17:23:05 -0400

    Update sketch.js

--- 1.mapping-time/clock-1/sketch.js
+++ 1.mapping-time/clock-1/sketch.js
@@ -10,16 +10,16 @@ function draw(){
   noStroke()
 
   var now = clock(),
-      pctHours = now.progress.day;
+      pctHour = now.progress.day;
       pctMin = now.progress.hour;
       pctSec = now.progress.min;
 
   fill(0, 0, 0)
-  circle(80, 200, 100*pctHours)
+  circle(80, 200, 100 * pctHour)
 
   fill(128, 128, 128)
-  circle(200, 200, 100*pctMin )
+  circle(200, 200, 100 * pctMin)
 
   fill(220, 220, 220)
-  circle(320, 200, 100*pctSec )
+  circle(320, 200, 100 * pctSec)
 }
